91661924388d158e72eb88a2b03e38948fbede1e
Benjamin Sehl <ben.sehl@shopify.com>
2024-08-07 17:46:49 -0400

    Lazy load rain effect and three JS stuff

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,8 +1,12 @@
-import {Canvas} from '@react-three/fiber';
+import {Suspense, lazy} from 'react';
 import {defer, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
 import {type MetaFunction} from '@remix-run/react';
 
-import RainEffect from '~/components/ShowerDoor';
+const Canvas = lazy(() =>
+  import('@react-three/fiber').then((module) => ({default: module.Canvas})),
+);
+
+const RainEffect = lazy(() => import('~/components/RainEffect'));
 
 export const meta: MetaFunction = () => {
   return [{title: 'Margin'}];
@@ -15,9 +19,26 @@ export async function loader(args: LoaderFunctionArgs) {
 export default function Homepage() {
   return (
     <div className="home" style={{width: '100%', height: '100vh'}}>
-      <Canvas style={{width: '100%', height: '100%'}}>
-        <RainEffect />
-      </Canvas>
+      <Suspense fallback={<BackgroundFallback />}>
+        <Canvas style={{width: '100%', height: '100%'}}>
+          <RainEffect />
+        </Canvas>
+      </Suspense>
     </div>
+  );
+}
+
+function BackgroundFallback() {
+  return (
+    <div
+      style={{
+        backgroundImage:
+          'url(https://cdn.shopify.com/s/files/1/0817/9308/9592/files/background-image.jpg?v=1720660332)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '100%',
+      }}
+    ></div>
   );
 }
